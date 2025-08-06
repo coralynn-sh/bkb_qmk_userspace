1f8cc7795c73dab5b30ec5b0d46581939f7c054f
Coralynn Colombe <colombe@coralynn.net>
2025-08-06 09:56:36 -0400

    2025-08-06T09:53:42-04:00

--- keyboards/bastardkb/charybdis/4x6/keymaps/coralynn/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/coralynn/keymap.c
@@ -152,7 +152,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_SOLO_L] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-       XXXXXXX,  KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, CM_NORM, XXXXXXX, XXXXXXX,
+       XXXXXXX,  KC_GRV, XXXXXXX, KC_LEFT, KC_MPLY, KC_RGHT,    XXXXXXX, XXXXXXX, XXXXXXX, CM_NORM, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_TAB,    KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
